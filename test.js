9dac6807a9801158b636af4d184d4990dd4fe6e7
ningrong <ningrong@minieye.cc>
2018-12-25 22:39:22 +0800

    修改测试，添加column nullable 的字段，并且查询 is null

--- test.js
+++ test.js
@@ -1,13 +1,18 @@
 const assert = require('assert');
 const mysql_dbc_table = require('./index');
+const db_util = require('./db_util');
 
 const DbTable = mysql_dbc_table.DbTable;
+const log = console.log.bind(console);
+const dbc = mysql_dbc_table.initDbc();
+log('[mysqlURI]', dbc.uri);
 
 class User extends DbTable {
     static fields() {
         return {
             id: {fmt: parseInt},
-            name: {fmt: String, default: ''},
+            name: {fmt: db_util.trimString, default: ''},
+            note: {fmt: db_util.varStr, default: null},
             created_time: {fmt: (...args) => new Date(...args)},
             modified_time: {fmt: (...args) => new Date(...args)},
             deleted: {fmt: Boolean, default: false},
@@ -15,42 +20,50 @@ class User extends DbTable {
     }
 }
 
+const sql_create_table = `
+    CREATE TABLE \`user\` (
+      \`id\` int(11) NOT NULL AUTO_INCREMENT,
+      \`name\` varchar(64) NOT NULL,
+      \`note\` varchar(32) default NULL,
+      \`created_time\` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      \`modified_time\` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+      \`deleted\` boolean DEFAULT false,
+      PRIMARY KEY (\`id\`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+`;
 
-const testMain = async () => {
-    const log = console.log.bind(console);
-    const dbc = mysql_dbc_table.initDbc();
-    log('[mysqlURI]', dbc.uri);
-    // mysql://root:@localhost:3306/test
 
-    // init table
-    const db_user = new User('user', dbc);
-    log(DbTable.name, User.name);
-    log('info', db_user);
-    log('toString', String(db_user));
-    log('toJSON', JSON.stringify(db_user));
+// testDropTable().then().catch();
+const testEnsureExist = async (db_model) => {
+    log('toString', String(db_model));
+    log('toJSON', JSON.stringify(db_model));
     log('showTables', await dbc.showTablesAsync());
-    log('showColumns', await db_user.showColumnsAsync())
 
-    const existed = await db_user.existAsync();
+    const existed = await db_model.existAsync();
     if (!existed) {
-        const sql_create_table = `
-            CREATE TABLE \`user\` (
-              \`id\` int(11) NOT NULL AUTO_INCREMENT,
-              \`name\` varchar(64) NOT NULL,
-              \`created_time\` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-              \`modified_time\` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-              \`deleted\` boolean DEFAULT false,
-              PRIMARY KEY (\`id\`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-        `;
         const s0 = await mysql_dbc_table.dbSqlAsync(dbc, sql_create_table);
         assert.equal(s0[0].serverStatus, 2);
-        console.log('create table<user>', s0);
-
-        const s1 = await db_user.existAsync();
+        log(`create table<${db_model.name}>`, s0);
+        const s1 = await db_model.existAsync();
         assert.equal(s1, true);
     }
+}
 
+const testDrop = async () => {
+    const sql_drop_table = `Drop Table \`user\`;`
+    const state = await mysql_dbc_table.dbSqlAsync(dbc, sql_drop_table)
+    log('drop!!!', sql_drop_table, state);
+}
+
+
+const testMain = async () => {
+    // mysql://root:@localhost:3306/test
+    // init table
+    const db_user = new User('user', dbc);
+    // log('info', db_user);
+    await testEnsureExist(db_user);
+    log(DbTable.name, User.name);
+    log('showColumns', await db_user.showColumnsAsync())
     // test model
     const ts = new Date().getTime();
     const u = {name: `tester_${ts}`};
@@ -79,12 +92,6 @@ const testMain = async () => {
     assert.equal(s8, 1);
     assert.equal(s9, null);
     assert.equal(s10.deleted, true);
-
-    if (s7 >= 2) {
-        const cnt = parseInt(s7 / 2);
-        const s11 = await db_user.findLimitAsync(cnt);
-        assert.equal(s11.length, cnt);
-    }
 
     // test update
     const ur = await db_user.findOneAsync({deleted: true}, false);
@@ -118,11 +125,14 @@ const testMain = async () => {
     const ds = await db_user.queryAsync(qry);
     console.log(222, ds);
 
-    const d3 = await db_user.upsertManyAsync(ds);
-    console.log(333, d3)
+    if (ds.length > 0) {
+        const d3 = await db_user.upsertManyAsync(ds);
+        console.log(333, d3)
 
-    const d4 = await db_user.replaceManyAsync(ds);
-    console.log(444, d4);
+        const d4 = await db_user.replaceManyAsync(ds);
+        console.log(444, d4);
+    }
+
 
     const d5 = await db_user.replaceOneAsync({name: "admin"});
     console.log(555, d5);
@@ -130,15 +140,56 @@ const testMain = async () => {
     const d6 = await db_user.replaceOneAsync({name: `admin_${d5_id}`, id: d5_id});
     console.log(666, d6);
 
-    const d7 = await db_user.disableAsync({id:d5_id});
+    const d7 = await db_user.disableAsync({id: d5_id});
     console.log(777, d7)
 
-    const d8 = await db_user.enableAsync({id:d5_id});
+    const d8 = await db_user.enableAsync({id: d5_id});
     console.log(888, d8)
 
-    const d9 = await db_user.deleteAsync({id:d5_id});
+    const d9 = await db_user.deleteAsync({id: d5_id});
     console.log(999, d9)
+
+    // eq null
+    const d10 = await db_user.queryAsync({opts: {eq: {note: null}}})
+    console.log(1000, d10.length, d10[0])
+    assert.equal(d10.length, 0)
+
+    // is null
+    const d11 = await db_user.queryAsync({opts: {is: {note: null}}})
+    console.log(1004, d11.length)
+
+    const d12 = await db_user.findAsync({note: null}, false)
+    console.log(1005, d12.length)
+    assert.equal(d12.length, d11.length)
+
+    const d13 = await db_user.findOneAsync({note: null}, false)
+    console.log(1006, d13)
+
+    const d14 = await db_user.findOneAsync({note: undefined}, false)
+    console.log(1007, d14)
+
+    const d15 = await db_user.findAsync();
+    console.log(1008, d15.length, d15)
+
+    if (d15.length > 0) {
+        const d16 = await db_user.updateAsync({id: d15[0].id}, {note: 'test undefined'});
+        console.log(1010, d16)
+    }
+
+    const d17 = await db_user.findOneAsync({note: undefined}, false)
+    console.log(1011, d17)
 };
 
+const testUtil = () => {
+    let k = db_util.varStr(undefined);
+    let k1 = db_util.varStr('undefine');
+    let k2 = db_util.varStr('undefined');
+    let k3 = db_util.varStr('None');
+    let k4 = db_util.varStr('NULL');
+    console.log(k, k1, k2, k3, k4)
+}
 
+
+// testDrop().then().catch();
 testMain().then().catch();
+testUtil();
